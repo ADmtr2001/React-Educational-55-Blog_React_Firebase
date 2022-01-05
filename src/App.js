9a5146c9fd9d0bcd593ff7a4aaf23c1700e89739
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-01-05 15:19:03 +0200

    add basic auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import React, {useState} from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
 import './App.css';
-
+import {signOut} from 'firebase/auth';
+import {auth} from "./firebase-config";
 
 const App = () => {
+	const [isAuth, setIsAuth] = useState(false);
+
+	const signUserOut = () => {
+		signOut(auth).then(() => {
+			localStorage.clear();
+			setIsAuth(false);
+			window.location.pathname = '/login';
+		})
+	};
+
 	return (
 		<Router>
 			<nav>
 				<Link to='/'>Home</Link>
 				<Link to='/createpost'>Create Post</Link>
-				<Link to='/login'>Login</Link>
+				{!isAuth ? <Link to='/login'>Login</Link> : <button onClick={signUserOut}>Logout</button>}
 			</nav>
 			<Routes>
 				<Route path='/' element={<Home/>}/>
 				<Route path='/createpost' element={<CreatePost/>}/>
-				<Route path='/login' element={<Login/>}/>
+				<Route path='/login' element={<Login setIsAuth={setIsAuth}/>}/>
 			</Routes>
 		</Router>
 	);
