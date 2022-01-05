80d20bd721089157391757269121146508ce99ff
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-01-05 16:24:26 +0200

    add create post logic

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {auth, db} from "../firebase-config";
+import {collection, addDoc} from 'firebase/firestore';
+import {useNavigate} from "react-router-dom";
 
-const CreatePost = () => {
+const CreatePost = ({isAuth}) => {
+	const [title, setTitle] = useState('');
+	const [postText, setPostText] = useState('');
+
+	const postsCollectionRef = collection(db, 'posts');
+	const navigate = useNavigate();
+
+	const createPost = async () => {
+		await addDoc(postsCollectionRef, {
+			title,
+			postText,
+			author: {
+				name: auth.currentUser.displayName,
+				id: auth.currentUser.uid,
+			}
+		});
+		navigate('/');
+	}
+
+	useEffect(() => {
+		if (!isAuth) {
+			navigate('/login');
+		}
+	}, []);
+
 	return (
-		<div>
-			CreatePost
+		<div className='createPostPage'>
+			<div className='cpContainer'>
+				<h1>Create A Post</h1>
+				<div className='inputGp'>
+					<label>Title:</label>
+					<input placeholder='Title...' value={title}
+								 onChange={(e) => setTitle(e.target.value)}/>
+				</div>
+				<div className='inputGp'>
+					<label>Post:</label>
+					<textarea placeholder='Post...' value={postText}
+										onChange={(e) => setPostText(e.target.value)}/>
+				</div>
+				<button onClick={createPost}>Submit Post</button>
+			</div>
 		</div>
 	);
 };
