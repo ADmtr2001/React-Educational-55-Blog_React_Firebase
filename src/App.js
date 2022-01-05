32ddd286ecd99d6930537dbe25b59cf37ac24364
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-01-05 17:10:21 +0200

    add data fetching and post rendering

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {signOut} from 'firebase/auth';
 import {auth} from "./firebase-config";
 
 const App = () => {
-	const [isAuth, setIsAuth] = useState(false);
+	const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'));
 
 	const signUserOut = () => {
 		signOut(auth).then(() => {
@@ -35,7 +35,7 @@ const App = () => {
 					</>}
 			</nav>
 			<Routes>
-				<Route path='/' element={<Home/>}/>
+				<Route path='/' element={<Home isAuth={isAuth}/>}/>
 				<Route path='/createpost' element={<CreatePost isAuth={isAuth}/>}/>
 				<Route path='/login' element={<Login setIsAuth={setIsAuth}/>}/>
 			</Routes>
